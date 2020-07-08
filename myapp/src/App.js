43e5e2b0e013f77bced6d93b0bcda901f15a5331
UbuntuUser <irvingdelgado6@gmail.com>
2020-07-07 22:37:14 -0500

    First commit

---
+++ myapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style.css';
+import TodoItem from './TodoItem';
+
+//This is a component called App.
+//This is the main component.
+function App() {
+  return (
+    <div className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />  
+    </div>
+  );
+}
+
+export default App;
